19ba87be6609a61d2c92a0aca0afec44d40f8d29
Terence Brewer <terencedbrewer@gmail.com>
2026-04-22 21:55:23 -0600

    initial commit

---
+++ cli/lib/semantic_search.py
@@ -0,0 +1,11 @@
+from sentence_transformers import SentenceTransformer
+
+class SemanticSearch:
+    def __init__(self):
+        self.model = SentenceTransformer('all-MiniLM-L6-v2')
+
+
+def verify_model():
+    ss = SemanticSearch()
+    print(f"Model loaded: {ss.model}")
+    print(f"Max sequence length: {ss.model.max_seq_length}")
